cd9e13db86aae67fe24433256bd878bdb3fc39ac
Peter Iskander <petermagdyiskander@gmail.com>
2023-02-02 15:10:52 +0200

    handle evan case

--- src/main/java/com/example/groupservice/GroupController.java
+++ src/main/java/com/example/groupservice/GroupController.java
@@ -1,7 +1,10 @@
 package com.example.groupservice;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +14,17 @@ public class GroupController {
     @Autowired
     private GroupService groupService;
 
-    @RequestMapping("/users/{userProfile}")
-    public String getUserID(@PathVariable String userProfile) {
-        return groupService.getId(userProfile);
+    // @RequestMapping("/users/{userProfile}")
+    // public String getUserID(@PathVariable String userProfile) {
+    // return groupService.getId(userProfile);
+    // }
+
+    @RequestMapping("/users")
+    public String getUserId(@RequestBody Map<String, String> json) {
+        // json = {
+        // userProfile: "https://www.facebook.com/profileHandler"
+        // }
+        return groupService.getId(json.get("userProfile"));
     }
 
 }
--- src/main/java/com/example/groupservice/GroupService.java
+++ src/main/java/com/example/groupservice/GroupService.java
@@ -11,9 +11,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class GroupService {
     public String getId(String userProfile) {
+        if (hasIdInURL(userProfile)) {
+            return userProfile.split("=")[1];
+        }
+        return getIdUsingNormalURL(userProfile);
+    }
+
+    boolean hasIdInURL(String userProfile) {
+        return userProfile.contains("profile.php");
+    }
+
+    String getIdUsingNormalURL(String userProfile) {
         Response req;
         String id = "";
         try {
+
             String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36";
 
             req = Jsoup.connect("https://m.facebook.com/login/async/?refsrc=https%3A%2F%2Fm.facebook.com%2F&lwv=100")
@@ -21,21 +33,34 @@ public class GroupService {
                     .method(Method.POST).data("email", "asd").data("pass", "asd")
                     .followRedirects(true).execute();
 
-            Document d = Jsoup.connect("https://facebook.com/" + userProfile).userAgent(userAgent)
+            Document d = Jsoup.connect(userProfile).userAgent(userAgent)
                     .cookies(req.cookies()).get();
+
             Elements metaTags = d.head().select("meta[content]");
             for (Element metaTag : metaTags) {
                 if (metaTag.attr("content").contains("profile")) {
                     return metaTag.attr("content").split("profile", 0)[1].substring(1);
                 }
             }
-            if(id.equals("")){
-                return d.head().toString();
+            if (id.equals("")) {
+                return uniqueURLS(d);
             }
             return id;
         } catch (Exception e) {
 
             return e.toString();
         }
+    }
+
+    String uniqueURLS(Document d) {
+        String id = "";
+        String lookUpTable = "0123456789";
+        for (int i = 0; i < d.html().split("\"entity_id\"")[1].split(",")[0].length(); i++) {
+            if (lookUpTable.contains(String.valueOf(d.html().split("\"entity_id\"")[1].split(",")[0].charAt(i)))) {
+                System.out.println(String.valueOf(d.html().split("\"entity_id\"")[1].split(",")[0].charAt(i)));
+                id += (String.valueOf(d.html().split("\"entity_id\"")[1].split(",")[0].charAt(i)));
+            }
+        }
+        return id;
     }
 }
